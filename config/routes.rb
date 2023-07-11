75917d37e7ba0f902d2a8346ab0a5034bf4bc2f0
Titus Mumo <titusmumo@gmail.com>
2023-07-11 17:24:26 +0300

    creating route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources: person
+  resources: people
 
   get 'render/index'
   root 'render#index'
